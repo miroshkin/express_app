741a8b3db74e8fd791edbb5882941d6adc0fa5c3
Мирошкин Антон <a.miroshkin@mxmgt.com>
2018-05-17 16:48:40 +0100

    Add module routing

--- app.js
+++ app.js
@@ -10,43 +10,39 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('index', {name});
-  } else {
-    res.redirect('/hello');
-  }
+const mainRoutes = require('./routes');
+const cardRoutes = require('./cards');
+app.use(mainRoutes);
+app.use(cardRoutes);
+
+app.use((req,res,next)=>{
+    console.log('Hello');
+    const err = new Error('Oh no!');
+    err.status = 500;
+    next(err);
 });
 
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: "TestUserName"});
+app.use((req,res,next)=>{
+    console.log('world');
+    next();
 });
 
-app.get('/sandbox', (req, res) => {
 
-    res.render('sandbox', {"message" : message});
+
+app.use((req,res,next)=>{
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
 });
 
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('hello');
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if(name){
-      res.redirect('/');
-    } else {
-      res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
+app.use((err,req,res,next) =>{
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error', err);
 });
 
 app.listen(3000, () => {
   console.log('The application is running on localhost: 3000');
 });
+
+module.exports = router;
